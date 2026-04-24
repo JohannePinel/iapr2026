27f59761e44b1bfae2ac0036c9d0bd3821718865
Johanne Pinel <johanne.pinel@epfl.ch>
2026-04-24 17:08:47 +0200

    tests avec hsv et rgb

---
+++ Project/src/Project_utils.py
@@ -0,0 +1,89 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from typing import Callable
+from datetime import datetime
+
+
+# Plot color space distribution 
+def plot_thresholded_image(
+    img: np.ndarray,
+    func: Callable,
+    title: str,
+):
+    """
+    Plot the original image and its thresholded version
+
+    Args
+    ----
+    img: np.ndarray (M, N, 3)
+        Input image of shape MxNx3.
+    func: Callable
+        Thresholded image.
+    title: str
+        Title of the plot
+    """
+
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+    axes[0].imshow(img)
+    axes[1].imshow(func(img), interpolation=None)
+    [a.axis('off') for a in axes]
+    plt.suptitle(title)
+    plt.tight_layout()
+
+
+# Plot color space distribution 
+def plot_colors_histo(
+    img: np.ndarray,
+    func: Callable,
+    labels: list[str],
+):
+    """
+    Plot the original image (top) as well as the channel's color distributions (bottom).
+
+    Args
+    ----
+    img: np.ndarray (M, N, C)
+        Input image of shape MxN and C channels.
+    func: Callable
+        A callable function that extracts D channels from the input image
+    labels: list of str
+        List of D labels indicating the name of the channel
+    """
+
+    # Extract colors
+    channels = func(img=img)
+    C2 = len(channels)
+    M, N, C1 = img.shape
+    fig = plt.figure(figsize=(16, 10))
+    gs = fig.add_gridspec(3, C2)
+
+    # Use random seed to downsample image colors (increase run speed - 10%)
+    mask = np.random.RandomState(seed=0).rand(M, N) < 0.1
+    
+    # Plot base image
+    ax = fig.add_subplot(gs[:2, :])
+    ax.imshow(img)
+    # Remove axis
+    ax.axis('off')
+    ax1 = fig.add_subplot(gs[2, 0])
+    ax2 = fig.add_subplot(gs[2, 1])
+    ax3 = fig.add_subplot(gs[2, 2])
+
+    # Plot channel distributions
+    ax1.scatter(channels[0][mask].flatten(), channels[1][mask].flatten(), c=img[mask]/255, s=1, alpha=0.1)
+    ax1.set_xlabel(labels[0])
+    ax1.set_ylabel(labels[1])
+    ax1.set_title("{} vs {}".format(labels[0], labels[1]))
+    ax2.scatter(channels[0][mask].flatten(), channels[2][mask].flatten(), c=img[mask]/255, s=1, alpha=0.1)
+    ax2.set_xlabel(labels[0])
+    ax2.set_ylabel(labels[2])
+    ax2.set_title("{} vs {}".format(labels[0], labels[2]))
+    ax3.scatter(channels[1][mask].flatten(), channels[2][mask].flatten(), c=img[mask]/255, s=1, alpha=0.1)
+    ax3.set_xlabel(labels[1])
+    ax3.set_ylabel(labels[2])
+    ax3.set_title("{} vs {}".format(labels[1], labels[2]))
+        
+    plt.tight_layout()
+
